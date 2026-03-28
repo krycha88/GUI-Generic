f54c8df8c94db0fa074676b385c593a56c2030f8
krycha88 <krystian.franieczek@gmail.com>
2026-03-28 18:33:35 +0100

    Update SuplaWebServer.cpp

--- src/SuplaWebServer.cpp
+++ src/SuplaWebServer.cpp
@@ -182,8 +182,8 @@ void SuplaWebServer::sendHeaderEnd() {
 #ifdef ARDUINO_ARCH_ESP8266
     tcpCleanup();
     httpServer->client().flush();
-#elif ARDUINO_ARCH_ESP32
-    httpServer->client().clear();
+// #elif ARDUINO_ARCH_ESP32
+//     httpServer->client().clear();
 
 #endif
     httpServer->client().stop();
